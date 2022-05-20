b3c3c82fb25f4b28bef7747eedb102f7b23aa87e
BruceAlves <brucewallace144@gmail.com>
2022-05-20 16:11:55 -0300

    Add project files.

---
+++ Consulta Cep/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consulta_Cep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu cep");
+            string cep = Console.ReadLine();
+
+            // buscando resposta no site do via cep
+            HttpClient cliente = new HttpClient();
+            var respostaJson = cliente.GetAsync($"https://viacep.com.br/ws/{cep}/json/").Result;
+
+            //convertendo resposta em string de json
+            string jsonString = respostaJson.Content.ReadAsStringAsync().Result;
+
+            //convertendo o string de json em objeto
+            RespostaVIaCep resposta = JsonConvert.DeserializeObject<RespostaVIaCep>(jsonString);
+
+            Console.WriteLine(resposta.Estado);
+            Console.WriteLine(resposta.Cidade);
+            Console.WriteLine(resposta.Bairro);
+            Console.WriteLine(resposta.Rua);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Consulta Cep/RespostaVIaCep.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consulta_Cep
+{
+    internal class RespostaVIaCep
+    {
+        [JsonProperty("cep")]
+        public string Cep { get; set; }
+
+        [JsonProperty("logradouro")]
+        public string Rua { get; set; }
+
+        [JsonProperty("bairro")]
+        public string Bairro { get; set; }
+
+        [JsonProperty("localidade")]
+        public string Cidade { get; set; }
+
+        [JsonProperty("uf")]
+        public string Estado { get; set; }
+
+
+    }
+}
